48ba689916d0ee87fc0d39787bcfe270d14ad53b
Tang-Zhichao <65081848+Tang-Zhichao@users.noreply.github.com>
2024-01-25 22:18:24 +0800

    New C++ version without the Eigen Library

---
+++ Integral_Lattice_without_Eigen.cpp
@@ -0,0 +1,260 @@
+// require latest C++ compling standard
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <array>
+#include <vector>
+#include <deque>
+#include <cmath>
+#include <chrono>
+#include <algorithm>
+#include <initializer_list>
+
+template<typename T>
+class INTEGRAL_LATTICE{
+    protected:
+    class group_element{
+        public:
+        std::vector<T> element;
+        int order;
+        const bool operator<(const group_element& other) const{return this->order < other.order;}
+        const bool operator>(const group_element& other) const{return this->order > other.order;}
+    };
+    private:
+    std::vector<std::vector<T>> intersection_matrix;
+    int rank;
+    int det;
+    std::vector<group_element> group_list;
+    std::vector<group_element> generator_list;
+    std::vector<std::vector<T>> sub_matirx(const int& i, const std::vector<std::vector<T>>& input_matrix){
+        std::vector<std::vector<T>> result_matrix;
+        for (int m = 0; m < input_matrix.size()-1; m++){
+            std::vector<int> temp;
+            for (int n = 0; n < input_matrix.size()-1; n++){
+                if (n < i) temp.emplace_back(input_matrix[m+1][n]);
+                else       temp.emplace_back(input_matrix[m+1][n+1]);
+            }
+            result_matrix.emplace_back(temp);
+        }
+        return result_matrix;
+    }
+    int determinant(const std::vector<std::vector<T>>& input_matrix){
+        int rank = input_matrix.size();
+        int result = 0;
+        if (rank == 1){
+            return input_matrix[0][0];
+        }else if (rank == 2){
+            return input_matrix[0][0]*input_matrix[1][1] - input_matrix[0][1]*input_matrix[1][0];
+        }else if (rank == 3){
+            result += input_matrix[0][0] * input_matrix[1][1] * input_matrix[2][2];
+            result -= input_matrix[0][0] * input_matrix[2][1] * input_matrix[1][2];
+            result -= input_matrix[1][0] * input_matrix[0][1] * input_matrix[2][2];
+            result += input_matrix[1][0] * input_matrix[2][1] * input_matrix[0][2];
+            result += input_matrix[2][0] * input_matrix[0][1] * input_matrix[1][2];
+            result -= input_matrix[2][0] * input_matrix[1][1] * input_matrix[0][2];
+            return result;
+        }else{
+            for (int i = 0; i < input_matrix.size(); i++){
+                if (i % 2 == 0) result += input_matrix[0][i] * determinant(sub_matirx(i,input_matrix));
+                else            result -= input_matrix[0][i] * determinant(sub_matirx(i,input_matrix));
+            }
+            return result;
+        }
+    }
+    const int the_order_of(const std::vector<T>& input_vector) const{
+        for (int i = 1; i <= std::abs(det); i++){
+            int t = 0;
+            for (int k = 0; k < rank; k++)
+                if ((input_vector[k] * i) % det == 0) t++;
+            if (t == rank) return i;
+        }
+        return 0;
+    }
+    template<typename M>
+    const bool in_integral_dual(const M& input_vector) const{
+        for (int i = 0; i < rank; i++){
+            int t = 0;
+            for (int j = 0; j < rank; j++) t += input_vector[j] * intersection_matrix[i][j];
+            if (t % det != 0) return false;
+        }
+        return true;
+    }
+    std::vector<group_element> group_list_checker(){
+        int length = rank;
+        int lower_bound = 0;
+        int upper_bound = det;
+        std::vector<group_element> result;
+        result.reserve(int(std::pow(upper_bound - lower_bound,length)));
+        std::vector<T> arr(length,lower_bound);
+        int position = length-1;
+        ready_flag:
+        if (in_integral_dual(arr)) result.emplace_back(group_element(arr,the_order_of(arr)));
+        not_ready_flag:
+        arr[position] = arr[position]+1;
+        if (arr[position] != upper_bound){
+            position = length-1;
+            goto ready_flag;
+        }else{
+            arr[position] = lower_bound;
+            position--;
+            if (position != -1) goto not_ready_flag;
+            else return result;
+        }
+    }
+    const bool in_coset_with(const group_element& a, const group_element& b,
+                            const std::vector<group_element>& subgroup_generators) const{
+        int length = subgroup_generators.size();
+        int lower_bound = 0;
+        int upper_bound = det;
+        std::vector<T> arr(length,lower_bound);
+        int position = length-1;
+        bool t;
+        ready_flag:
+        t = true;
+        std::vector<T> test_coordinates;
+        test_coordinates.reserve(rank);
+            for (int i = 0; i < rank; i++) test_coordinates.push_back(a.element[i] - b.element[i]);
+            for (int j = 0; j < length; j++){
+                for (int k = 0; k < rank; k++) test_coordinates[k] = test_coordinates[k] + arr[j] * ((subgroup_generators[j].element)[k]);
+            }
+            for (auto cood : test_coordinates){
+                if (cood % det != 0){
+                    t = false;
+                    break;
+                }
+            }
+            if (t == true) return true;
+        not_ready_flag:
+        arr[position] = arr[position]+1;
+        if (arr[position] != upper_bound){
+            position = length-1;
+            goto ready_flag;
+        }else{
+            arr[position] = lower_bound;
+            position--;
+            if (position != -1) goto not_ready_flag;
+            else return false;
+        }
+    }
+    std::vector<group_element> generators_extractor(){
+        std::vector<group_element> found_generators;
+        std::vector<group_element> remain_list = group_list;
+        found_generators.push_back(*std::max_element(remain_list.begin(),remain_list.end()));
+        int products_of_orders = found_generators.front().order;
+        while (products_of_orders < std::abs(det)){
+            std::vector<group_element> result_list;
+            std::vector<std::vector<group_element>> cosets;
+            for (auto item : remain_list){
+                std::vector<group_element> new_coset;
+                bool coset_found = false;
+                for (auto& coset : cosets){
+                    if (in_coset_with(item,coset[0],found_generators) == true){
+                        coset.push_back(item);
+                        coset_found = true;
+                        break;
+                    }
+                }
+                if (coset_found == false){
+                    new_coset.push_back(item);
+                    cosets.push_back(new_coset);
+                }
+            }
+            for (auto coset : cosets) result_list.push_back(*std::min_element(coset.begin(),coset.end()));
+            remain_list = result_list;
+            if (remain_list.size() != 1){
+                found_generators.push_back(*std::max_element(remain_list.begin(),remain_list.end()));
+                products_of_orders *= found_generators.back().order;
+            }else break;
+        }
+        return found_generators;
+    }
+    public:
+    INTEGRAL_LATTICE(std::initializer_list<T> input_list){
+        std::vector<T> lists = input_list;
+        rank = int(std::sqrt(input_list.size()));
+        for (int j = 0; j < rank; j++){
+            std::vector<T> temp;
+            temp.reserve(rank);
+            for (int k = 0; k < rank; k++) temp.push_back(lists[rank*j + k]);
+            intersection_matrix.push_back(temp);
+        }
+        det = determinant(intersection_matrix);
+        group_list = group_list_checker();
+        generator_list = generators_extractor();
+    }
+    INTEGRAL_LATTICE(const std::vector<T>& input_list){
+        rank = int(std::sqrt(input_list.size()));
+        for (int j = 0; j < rank; j++){
+            std::vector<T> temp;
+            temp.reserve(rank);
+            for (int k = 0; k < rank; k++) temp.push_back(input_list[rank*j + k]);
+            intersection_matrix.push_back(temp);
+        }
+        det = determinant(intersection_matrix);
+        group_list = group_list_checker();
+        generator_list = generators_extractor();
+    }
+    const int get_rank()const{return rank;}
+    const int get_det() const{return det;}
+    void get_matrix(){
+        for (auto i : intersection_matrix){
+            for (auto j : i) std::cout << j << " ";
+            std::cout << std::endl;
+        }
+    }
+    void show_info(){
+        std::cout << "The intersection form with the following intersection matrix" << std::endl;
+        get_matrix();
+        std::cout << "is of rank " << rank << " and determinant (or discriminant) " << det << std::endl;
+        auto info_loop = [](std::vector<group_element>& input_list){
+            for (auto i : input_list){
+                std::cout << "[ ";
+                for (auto j : i.element) std::cout << j << " ";
+                std::cout << "] with order " << i.order << std::endl;
+            }
+        };
+        std::cout << "whose integral dual is of the same size and with elements:" << std::endl;
+        info_loop(group_list);
+        std::cout << "A set of generators may be of size " << generator_list.size() << " with elements:" << std::endl;
+        info_loop(generator_list);
+    }
+};
+
+int main(){
+    auto start_time = std::chrono::steady_clock::now();
+
+    // INTEGRAL_LATTICE<int> A = {2,0,0,0,0,
+    //                            0,2,0,0,0,
+    //                            0,0,2,0,0,
+    //                            0,0,0,2,0,
+    //                            0,0,0,0,2};
+    // A.show_info();
+
+    // INTEGRAL_LATTICE<int> B = {2,  0,  0, -1,  0,  0,
+    //                            0,  2, -1,  0,  0,  0,
+    //                            0, -1,  2, -1,  0,  0,
+    //                           -1,  0, -1,  2, -1,  0,
+    //                            0,  0,  0, -1,  2, -1,
+    //                            0,  0,  0,  0, -1,  2};
+    // B.show_info();
+
+    // INTEGRAL_LATTICE<int> C = {2,  0,  0, -1,  0,  0,  0,
+    //                            0,  2, -1,  0,  0,  0,  0,
+    //                            0, -1,  2, -1,  0,  0,  0,
+    //                           -1,  0, -1,  2, -1,  0,  0,
+    //                            0,  0,  0, -1,  2, -1,  0,
+    //                            0,  0,  0,  0, -1,  2, -1,
+    //                            0,  0,  0,  0,  0, -1,  2};
+    // C.show_info();
+
+    int t = 5;
+    INTEGRAL_LATTICE<int> D = {3, 1, 1,
+                               1, 7, t,
+                               1, t, 13};
+    D.show_info();
+
+    auto end_time = std::chrono::steady_clock::now();
+    double duration_sec = std::chrono::duration<double>(end_time - start_time).count();
+    std::cout << "Total time cost: " << duration_sec << " secs." << std::endl;
+    std::cin.get();
+}
